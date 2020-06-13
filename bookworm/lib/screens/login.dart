78dc0cdc9e1c2ce2b841d3f50fadcb85c6060f45
BeatrizSilverio <63482161+BeatrizSilverio@users.noreply.github.com>
2020-06-13 14:21:23 -0300

    Atualizações

--- bookworm/lib/screens/login.dart
+++ bookworm/lib/screens/login.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,13 +16,62 @@ class LoginScreen extends StatelessWidget {
             'assets/images/fundoClaro.jpeg',
             fit: BoxFit.cover,
           ),
-          Column(
-            children: <Widget>[
-              Text('Login', style: Theme.of(context).textTheme.headline5,),
-            ],
+          Container(
+            color: Color.fromRGBO(255, 255, 255, 0.25),
+            child: Column(
+
+              children: <Widget>[
+                Container(child:
+                Text(
+                  'Login',
+                  style: Theme.of(context).textTheme.headline5,
+                ),
+                  padding: const EdgeInsets.only(top: 100, bottom: 50),
+                ),
+                Container(
+                  width: 320,
+                  padding: const EdgeInsets.only(bottom: 50),
+                  child: TextField(
+                    style: TextStyle(fontSize: 30, height: 1,   ),
+                    decoration: InputDecoration(
+                        labelText: 'Email',
+                        labelStyle: TextStyle(color: Color.fromRGBO(252, 252, 252, 1)),
+                        filled: true,
+                        fillColor: Color.fromRGBO(25, 50, 60, 0.85),
+                        border: OutlineInputBorder(
+                            borderRadius: const BorderRadius.all(Radius.circular(40))
+                        ),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 320,
+                  child: TextField(
+                    style: TextStyle(fontSize: 30, height: 1),
+                    decoration: InputDecoration(
+                      labelText: 'Senha',
+                        labelStyle: TextStyle(color: Color.fromRGBO(252, 252, 252, 1)),
+                        filled: true,
+                        fillColor: Color.fromRGBO(25, 50, 60, 0.85),
+                        border: OutlineInputBorder(
+                            borderRadius: const BorderRadius.all(Radius.circular(40))
+                        ),
+                  ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
     );
+  }
+}
+
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
   }
 }
