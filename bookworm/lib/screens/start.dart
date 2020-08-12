b4b3889dc66a24dcbac1fab105598c64a656bc7e
BeatrizSilverio <63482161+BeatrizSilverio@users.noreply.github.com>
2020-08-12 16:14:31 -0300

    efeito lá

--- bookworm/lib/screens/start.dart
+++ bookworm/lib/screens/start.dart
@@ -1,26 +1,52 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:flutter/services.dart';
 
-class StartScreen extends StatelessWidget {
+class StartScreen extends StatefulWidget {
+  @override
+  StartScreenState createState() => StartScreenState();
+}
+
+class StartScreenState extends State<StartScreen> {
+  @override
+    void navigationToNextPage(){
+
+    Navigator.pushReplacementNamed(context, '/LoginScreen');
+
+  }
+
+  startSplashScreenTimer() async {
+    var _duration = new Duration(seconds: 5);
+    return new Timer(_duration, navigationToNextPage);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    startSplashScreenTimer();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-          Image.asset(
-            'assets/images/fundo.jpeg',
-            fit: BoxFit.cover,
+    SystemChrome.setEnabledSystemUIOverlays([]);
+
+    return Stack(
+      fit: StackFit.expand,
+          children: <Widget>[
+            Image.asset(
+              'assets/images/fundo.jpeg',
+              fit: BoxFit.cover,
           ),
           Container(
-            color: Color.fromRGBO(255, 255, 255, 0.25),
-            child: Center(
-              child: Image.asset(
-                'assets/images/logo.png',
-              ),
+             color: Color.fromRGBO(255, 255, 255, 0.25),
+             child: Center(
+               child: Image.asset(
+                 'assets/images/logo.png',
+               ),
             ),
-          )
-        ],
-      ),
-    );
+           )
+         ],
+         );
   }
-}
+
+  }
