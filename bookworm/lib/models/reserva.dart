d2f815527bebe2a9177e6954c094cab39adae2ad
BeatrizSilverio <63482161+BeatrizSilverio@users.noreply.github.com>
2020-09-05 19:12:14 -0300

    get

--- bookworm/lib/models/reserva.dart
+++ bookworm/lib/models/reserva.dart
@@ -1,3 +1,7 @@
+import 'package:teste/http/connection.dart';
+import 'dart:convert';
+import 'package:http/http.dart';
+
 class Reserva {
   final int idProduto;
   final int idLeitor;
@@ -6,14 +10,24 @@ class Reserva {
   Reserva(this.idProduto, this.idLeitor, this.dataReserva);
 
   Reserva.fromJson(Map<String, dynamic> json)
-        : idProduto = json['idProduto'],
-          idLeitor = json['idLeitor'],
-          dataReserva = json['dataReserva'];
+      : idProduto = json['idProduto'],
+        idLeitor = json['idLeitor'],
+        dataReserva = json['dataReserva'];
 
   Map<String, dynamic> toJson() =>
-    {
-      'idProduto' : idProduto,
-      'idLeitor' : idLeitor,
-      'dataReserva' : dataReserva
-    };
+      {
+        'idProduto' : idProduto,
+        'idLeitor' : idLeitor,
+        'dataReserva' : dataReserva
+      };
+
+  Future<Reserva> getReserva() async{
+    final Response response = await client.get(baseUrl);
+    return Reserva.fromJson(jsonDecode(response.body));
+  }
+
+  Future<Reserva> putReserva(Reserva reserva) async{
+    final Response response = await client.put(baseUrl, body: reserva);
+    return Reserva.fromJson(jsonDecode(response.body));
+  }
 }
