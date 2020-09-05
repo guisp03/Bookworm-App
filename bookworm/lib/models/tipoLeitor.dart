cdd9c97fcff91098eef021699239f030fd3570ca
BeatrizSilverio <63482161+BeatrizSilverio@users.noreply.github.com>
2020-09-05 19:12:50 -0300

    get

--- bookworm/lib/models/tipoLeitor.dart
+++ bookworm/lib/models/tipoLeitor.dart
@@ -1,3 +1,7 @@
+import 'package:teste/http/connection.dart';
+import 'dart:convert';
+import 'package:http/http.dart';
+
 class TipoLeitor {
   final int idTipoLeitor;
   final String tipo;
@@ -9,7 +13,18 @@ class TipoLeitor {
         tipo = json['tipo'];
 
   Map<String, dynamic> toJson() => {
-        'idTipoLeitor': idTipoLeitor,
-        'tipo': tipo,
-      };
+    'idTipoLeitor': idTipoLeitor,
+    'tipo': tipo,
+  };
+
+  Future<TipoLeitor> getTipoLeitor() async{
+    final Response response = await client.get(baseUrl);
+    return TipoLeitor.fromJson(jsonDecode(response.body));
+  }
+
+  Future<TipoLeitor> putTipoLeitor(TipoLeitor tipoLeitor) async{
+    final Response response = await client.put(baseUrl, body: tipoLeitor);
+    return TipoLeitor.fromJson(jsonDecode(response.body));
+  }
+
 }
