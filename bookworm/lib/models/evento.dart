62f8f3b940469ba4f920a5e527344c912a237e2a
BeatrizSilverio <63482161+BeatrizSilverio@users.noreply.github.com>
2020-09-05 19:09:00 -0300

    get

--- bookworm/lib/models/evento.dart
+++ bookworm/lib/models/evento.dart
@@ -14,18 +14,28 @@ class Evento {
       );
 
   Evento.fromJson(Map<String, dynamic> json)
-        : idEvento = json['idEvento'],
-          titulo = json['titulo'],
-          descricao = json['descricao'],
-          responsavel = json['responsavel'];
+      : idEvento = json['idEvento'],
+        titulo = json['titulo'],
+        descricao = json['descricao'],
+        responsavel = json['responsavel'];
 
   Map<String, dynamic> toJson() =>
-    {
-      'idEvento' : idEvento,
-      'titulo' : titulo,
-      'descricao' : descricao,
-      'responsavel' : responsavel,
-    };
+      {
+        'idEvento' : idEvento,
+        'titulo' : titulo,
+        'descricao' : descricao,
+        'responsavel' : responsavel,
+      };
+
+  Future<Evento> getEvento() async{
+    final Response response = await client.get(baseUrl);
+    return Evento.fromJson(jsonDecode(response.body));
+  }
+
+  Future<Evento> putEvento(Evento evento) async{
+    final Response response = await client.put(baseUrl, body: evento);
+    return Evento.fromJson(jsonDecode(response.body));
+  }
 
   Future<List<Evento>> getEventos() async{
     final Response response = await client.get(baseUrl);
