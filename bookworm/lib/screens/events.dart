8c8b0d402a75a88e68f13c8768a8fe34a024529d
BeatrizSilverio <63482161+BeatrizSilverio@users.noreply.github.com>
2020-11-22 19:51:50 -0300

    Update events.dart

--- bookworm/lib/screens/events.dart
+++ bookworm/lib/screens/events.dart
@@ -1,17 +1,103 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:teste/components/clickableText.dart';
 import 'package:teste/components/pageModelInside.dart';
+import 'package:teste/models/evento.dart';
 
-class EventsScreen extends StatelessWidget {
+class EventsScreen extends StatefulWidget {
+  final String titulo;
+  final String descricao;
+  final String responsavel;
+  final String email;
+
+  const EventsScreen(this.titulo, this.descricao, this.responsavel, this.email);
+
+  @override
+  _EventsScreenState createState() => _EventsScreenState();
+}
+
+class _EventsScreenState extends State<EventsScreen> {
+  EventoClienteWeb eventoClienteWeb =  new EventoClienteWeb();
+  EventoWeb eventoWeb;
+  Future<EventoWeb> future;
+  List<Evento> eventos = [];
+  int page = 1;
+  int count = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    future = eventoClienteWeb.getEventoWeb(page);
+  }
+
   @override
   Widget build(BuildContext context) {
     return PageModelInsideScreen(
-      "Eventos",
-      RichText(
-        textAlign: TextAlign.left,
-        text: TextSpan(
-            text: 'Algum texto a ser feito ainda',
-            style: Theme.of(context).textTheme.subtitle1),
-      ),
-    );
-  }
-}
+      'Eventos',
+      SingleChildScrollView(
+        child: FutureBuilder<EventoWeb>(
+          future: future,
+          builder: (context,snapshot){
+           switch (snapshot.connectionState) {
+             case ConnectionState.none:
+                break;
+             case ConnectionState.waiting:
+               return CircularProgressIndicator();
+               break;
+             case ConnectionState.active:
+               break;
+             case ConnectionState.done:
+               if (snapshot.hasData) {
+                 eventoWeb = snapshot.data;
+                 count = count + eventoWeb.count;
+                 eventoWeb.eventos.forEach((element) => eventos.add(element));
+                 if(eventos.isNotEmpty) {
+                   return Column(
+                     children: <Widget>[
+                       MediaQuery.removePadding(
+                           context: context,
+                           removeTop: true,
+                           child: ListView.builder(
+                               scrollDirection: Axis.vertical,
+                               shrinkWrap: true,
+                               physics: ScrollPhysics(),
+                               itemCount: eventos.length,
+                               itemBuilder: (context, i) {
+                                 List<Evento> eventos;
+                                 return Column(
+                                   children: <Widget>[
+                                     Text(
+                                       "Eventos",
+                                     ),
+                                     Text('Título: ' + eventos[i].titulo),
+                                     Text('Descrição: ' + eventos[i].descricao),
+                                     Text('Responsável: ' + eventos[i].responsavel),
+                                     Text('Email: ' + eventos[i].email),
+                                   ],
+                                 );
+                                 }
+                           )
+                       ),
+                       eventoWeb.totalCount > count
+                           ? ClickableText(16, 16, () {
+                         setState(() {
+                           page++;
+                           future = eventoClienteWeb.getEventoWeb(page);
+                         });
+                       }, Text('Carregar mais'))
+                           : Text(''),
+                     ]
+                   );
+                       }
+                   }
+               return Text(
+                 'Falha ao buscar eventos'
+               );
+                   }
+                   return Text ('Falha desconhecida');
+          }
+                   )
+                   )
+                  );
+                 }
+                 }
