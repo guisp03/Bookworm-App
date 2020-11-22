e46a6096b582b4b6fe77e086c7afe0eac433c1bd
BeatrizSilverio <63482161+BeatrizSilverio@users.noreply.github.com>
2020-11-22 19:51:02 -0300

    Update evento.dart

--- bookworm/lib/models/evento.dart
+++ bookworm/lib/models/evento.dart
@@ -9,9 +9,8 @@ class Evento {
   final String responsavel;
   final String email;
 
-  Evento(this.idEvento, this.titulo, this.descricao, this.responsavel, this.email,
-
-      );
+  Evento(this.idEvento, this.titulo, this.descricao, this.responsavel,
+      this.email,);
 
   Evento.fromJson(Map<String, dynamic> json)
       : idEvento = json['idEvento'],
@@ -20,7 +19,7 @@ class Evento {
         responsavel = json['responsavel'],
         email = json['email'];
 
-  Map<String, dynamic> toJson() =>
+/* Map<String, dynamic> toJson() =>
       {
         'idEvento' : idEvento,
         'titulo' : titulo,
@@ -39,15 +38,44 @@ class Evento {
     return Evento.fromJson(jsonDecode(response.body));
   }
 
-  
+
 }
 
 class EventoWeb {
   Future<List<Evento>> getEventos() async{
-    final Response response = await client.get(baseUrl);
+    final Response response = await client.get("http://192.168.0.6:45457/eventos");
     final List<dynamic> decodedJson = jsonDecode(response.body);
     return decodedJson
         .map((dynamic json) => Evento.fromJson(json))
         .toList();
+  }*/
+}
+
+
+class EventoWeb {
+  final int totalCount;
+  final int count;
+  final List<Evento> eventos;
+
+  EventoWeb(
+      this.totalCount,
+      this.count,
+      this.eventos,
+      );
+
+  EventoWeb.fromJson(Map<String, dynamic> json)
+  : totalCount = json['total_count'],
+    count = json['count'],
+    eventos = (json['eventos'] as List).map((e) => Evento.fromJson(e)).toList();
+}
+
+class EventoClienteWeb {
+  Future<EventoWeb> getEventoWeb(int page) async {
+    final Response response = await client.get("http://192.168.0.6:45455/eventos");
+    if (response.statusCode == 200) {
+      return EventoWeb.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Falha ao buscar eventos!');
+    }
   }
 }
