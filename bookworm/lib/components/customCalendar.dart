ed6a5393832785f698019174646f0dffc8cc631e
BeatrizSilverio <63482161+BeatrizSilverio@users.noreply.github.com>
2020-12-01 17:53:02 -0300

    DatePicker

--- bookworm/lib/components/customCalendar.dart
+++ bookworm/lib/components/customCalendar.dart
@@ -3,165 +3,110 @@ import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:intl/intl.dart';
-import 'package:table_calendar/table_calendar.dart';
+import 'package:flutter_rounded_date_picker/rounded_picker.dart';
+import 'package:teste/models/produto.dart';
 
 class CustomCalendar extends StatefulWidget {
+  final int isbn;
+  final int reservas;
+
+  const CustomCalendar(
+    this.isbn,
+    this.reservas,
+  );
+
   @override
   _CustomCalendarState createState() => _CustomCalendarState();
 }
 
 class _CustomCalendarState extends State<CustomCalendar> {
-  CalendarController _controller;
+  ProdutoClienteWeb produtoClienteWeb = new ProdutoClienteWeb();
+  ReservaWeb reservaWeb;
+  Future<ReservaWeb> future;
+  int isbn;
+  int reservas;
+  int count = 0;
 
   @override
   void initState() {
     super.initState();
-    _controller = CalendarController();
+    future = produtoClienteWeb.getReservaWeb(isbn);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-          padding: EdgeInsets.zero,
-          margin: EdgeInsets.only(bottom: 10),
-          height: 295,
-          width: 380,
-          decoration: BoxDecoration(
-            border: Border(
-              bottom: BorderSide(
-                  width: 2, color: Color.fromRGBO(229, 229, 229, 0.5)),
-            ),
-          ),
-          child: TableCalendar(
-            locale: 'pt_BR',
-            calendarStyle: CalendarStyle(
-                todayColor: Colors.transparent,
-                selectedColor: Colors.transparent,
-                todayStyle: TextStyle(
-                  fontSize: 20,
-                ),
-                selectedStyle: TextStyle(
-                  fontSize: 20,
-                ),
-                weekendStyle: TextStyle(fontSize: 20, color: Colors.white),
-                weekdayStyle: TextStyle(
-                  fontSize: 20,
-                ),
-                outsideStyle: TextStyle(
-                    fontSize: 20, color: Color.fromRGBO(229, 229, 229, 0.5)),
-                outsideWeekendStyle: TextStyle(
-                  fontSize: 20,
-                  color: Color.fromRGBO(229, 229, 229, 0.5),
-                )),
-            headerStyle: HeaderStyle(
-              titleTextBuilder: (date, locale) =>
-                  DateFormat.MMMM(locale).format(date).toLowerCase(),
-              centerHeaderTitle: true,
-              titleTextStyle: Theme.of(context).textTheme.subtitle1,
-              formatButtonVisible: false,
-            ),
-            daysOfWeekStyle: DaysOfWeekStyle(
-              weekendStyle: TextStyle().copyWith(color: Colors.white),
-              dowTextBuilder: (date, locale) =>
-                  DateFormat.E(locale).format(date)[0],
-            ),
-            builders: CalendarBuilders(
-              selectedDayBuilder: (context, date, events) => Container(
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(
-                    width: 1,
-                    color: Colors.white,
-                  ),
-                ),
-                child: Text(
-                  date.day.toString(),
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                  ),
-                ),
-              ),
-              dowWeekdayBuilder: (context, date) => Container(
-                alignment: Alignment.center,
-                child: Text(
-                  date.toString().toUpperCase(),
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                  ),
-                ),
-              ),
-              dowWeekendBuilder: (context, date) => Container(
-                alignment: Alignment.center,
-                margin: EdgeInsets.zero,
-                child: Text(
-                  date.toString().toUpperCase(),
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                  ),
-                ),
-              ),
-            ),
-            calendarController: _controller,
-          ),
+    return RaisedButton(onPressed: () {
+      if (isbn == reservas) {
+        showDialog(
+            context: context,
+            builder: (c) => CupertinoAlertDialog(
+                  title: Text('Este livro está indisponível!'),
+                  actions: <Widget>[
+                    CupertinoDialogAction(
+                      child: Text('Ok'),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    )
+                  ],
+                ));
+      }
+      Future<DateTime> selectDay = showRoundedDatePicker(
+        context: context,
+        fontFamily: 'JosefinSans',
+        textNegativeButton: 'Cancelar',
+        textPositiveButton: 'Ok',
+        height: 340,
+        theme: ThemeData(primaryColor: Color.fromRGBO(0, 0, 0, 0)),
+        styleDatePicker: MaterialRoundedDatePickerStyle(
+          paddingDatePicker: EdgeInsets.only(left: 8, right: 8),
+          paddingActionBar: EdgeInsets.zero,
+          paddingDateYearHeader: EdgeInsets.zero,
+          textStyleDayButton:
+              TextStyle(fontSize: 0, color: Color.fromRGBO(25, 50, 60, 0.85)),
+          textStyleYearButton:
+              TextStyle(fontSize: 0, color: Color.fromRGBO(25, 50, 60, 0.85)),
+          textStyleCurrentDayOnCalendar: TextStyle(
+              color: Colors.white, fontSize: 20, fontFamily: 'JosefinSans'),
+          textStyleDayOnCalendar: TextStyle(
+              color: Colors.white, fontSize: 20, fontFamily: 'JosefinSans'),
+          textStyleDayOnCalendarSelected: TextStyle(
+              color: Colors.white, fontSize: 20, fontFamily: 'JosefinSans'),
+          textStyleDayHeader: TextStyle(
+              color: Colors.white, fontSize: 20, fontFamily: 'JosefinSans'),
+          textStyleMonthYearHeader: TextStyle(
+              color: Colors.white, fontSize: 20, fontFamily: 'JosefinSans'),
+          textStyleButtonNegative: TextStyle(
+              color: Colors.white, fontSize: 20, fontFamily: 'JosefinSans'),
+          textStyleButtonPositive: TextStyle(
+              color: Colors.white, fontSize: 20, fontFamily: 'JosefinSans'),
+          textStyleDayOnCalendarDisabled: TextStyle(
+              color: Colors.white, fontSize: 20, fontFamily: 'JosefinSans'),
+          paddingMonthHeader: EdgeInsets.all(28),
+          sizeArrow: 30,
+          colorArrowNext: Colors.white,
+          colorArrowPrevious: Colors.white,
+          marginLeftArrowPrevious: 12,
+          marginRightArrowNext: 12,
+          marginTopArrowNext: 12,
+          marginTopArrowPrevious: 12,
+          decorationDateSelected: BoxDecoration(
+              color: Color.fromRGBO(25, 50, 60, 0.85),
+              shape: BoxShape.circle,
+              border: Border.all(color: Colors.white, width: 1)),
+          backgroundPicker: Color.fromRGBO(25, 50, 60, 0.85),
+          backgroundActionBar: Color.fromRGBO(25, 50, 60, 0.85),
+          backgroundHeaderMonth: Color.fromRGBO(25, 50, 60, 0.85),
         ),
-        Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
-          Row(mainAxisAlignment: MainAxisAlignment.start, children: <Widget>[
-            Container(
-                margin: EdgeInsets.all(10),
-                width: 20,
-                height: 20,
-                decoration: BoxDecoration(
-                    color: Color.fromRGBO(158, 44, 74, 0.8),
-                    shape: BoxShape.circle,
-                    border: Border.all(color: Colors.white, width: 1))),
-            Text(
-              'Livro reservado',
-              style: Theme.of(context).textTheme.bodyText1,
-            ),
-          ]),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                  margin: EdgeInsets.all(10),
-                  width: 20,
-                  height: 20,
-                  decoration: BoxDecoration(
-                      color: Color.fromRGBO(233, 215, 88, 0.8),
-                      shape: BoxShape.circle,
-                      border: Border.all(color: Colors.white, width: 1))),
-              Text(
-                'Pode ocorrer a renovação',
-                style: Theme.of(context).textTheme.bodyText1,
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                  margin: EdgeInsets.all(10),
-                  width: 20,
-                  height: 20,
-                  decoration: BoxDecoration(
-                      color: Color.fromRGBO(252, 252, 252, 0.8),
-                      shape: BoxShape.circle,
-                      border: Border.all(color: Colors.white, width: 1))),
-              Text(
-                'Livro disponível',
-                style: Theme.of(context).textTheme.bodyText1,
-              ),
-            ],
-          ),
-        ]),
-      ],
-    );
+        styleYearPicker: MaterialRoundedYearPickerStyle(
+          textStyleYear: TextStyle(fontSize: 50, color: Colors.white),
+          heightYearRow: 0,
+        ),
+        initialDate: DateTime.now(),
+        firstDate: DateTime(DateTime.now().year),
+        lastDate: DateTime(DateTime.now().year + 1),
+        borderRadius: 16,
+      );
+    });
   }
 }
