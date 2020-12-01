a82524099eff71bbe8bf0175f76986e35401929f
BeatrizSilverio <63482161+BeatrizSilverio@users.noreply.github.com>
2020-12-01 18:56:52 -0300

    date picker

--- bookworm/lib/screens/product.dart
+++ bookworm/lib/screens/product.dart
@@ -3,6 +3,7 @@ import 'package:teste/components/clickableText.dart';
 import 'package:teste/components/customAlertDialog.dart';
 import 'package:teste/components/pageModelInside.dart';
 import 'package:teste/components/textWithIcon.dart';
+import 'package:flutter_rounded_date_picker/rounded_picker.dart';
 
 class ProductScreen extends StatefulWidget {
   final int id;
@@ -84,11 +85,104 @@ class _ProductScreenState extends State<ProductScreen> {
                                       ClickableText(
                                         0.0,
                                         0.0,
-                                        () => showDatePicker(
-                                            context: context,
-                                            initialDate: DateTime.now(),
-                                            firstDate: DateTime.now(),
-                                            lastDate: DateTime(3000)),
+                                        () => showRoundedDatePicker(
+                                          context: context,
+                                          fontFamily: 'JosefinSans',
+                                          textNegativeButton: 'Cancelar',
+                                          textPositiveButton: 'Ok',
+                                          height: 340,
+                                          theme: ThemeData(
+                                              primaryColor:
+                                                  Color.fromRGBO(0, 0, 0, 0)),
+                                          styleDatePicker:
+                                              MaterialRoundedDatePickerStyle(
+                                            paddingDatePicker: EdgeInsets.only(
+                                                left: 8, right: 8),
+                                            paddingActionBar: EdgeInsets.zero,
+                                            paddingDateYearHeader:
+                                                EdgeInsets.zero,
+                                            textStyleDayButton: TextStyle(
+                                                fontSize: 0,
+                                                color: Color.fromRGBO(
+                                                    25, 50, 60, 0.85)),
+                                            textStyleYearButton: TextStyle(
+                                                fontSize: 0,
+                                                color: Color.fromRGBO(
+                                                    25, 50, 60, 0.85)),
+                                            textStyleCurrentDayOnCalendar:
+                                                TextStyle(
+                                                    color: Colors.white,
+                                                    fontSize: 20,
+                                                    fontFamily: 'JosefinSans'),
+                                            textStyleDayOnCalendar: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 20,
+                                                fontFamily: 'JosefinSans'),
+                                            textStyleDayOnCalendarSelected:
+                                                TextStyle(
+                                                    color: Colors.white,
+                                                    fontSize: 20,
+                                                    fontFamily: 'JosefinSans'),
+                                            textStyleDayHeader: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 20,
+                                                fontFamily: 'JosefinSans'),
+                                            textStyleMonthYearHeader: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 20,
+                                                fontFamily: 'JosefinSans'),
+                                            textStyleButtonNegative: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 20,
+                                                fontFamily: 'JosefinSans'),
+                                            textStyleButtonPositive: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 20,
+                                                fontFamily: 'JosefinSans'),
+                                            textStyleDayOnCalendarDisabled:
+                                                TextStyle(
+                                                    color: Colors.white,
+                                                    fontSize: 20,
+                                                    fontFamily: 'JosefinSans'),
+                                            paddingMonthHeader:
+                                                EdgeInsets.all(28),
+                                            sizeArrow: 30,
+                                            colorArrowNext: Colors.white,
+                                            colorArrowPrevious: Colors.white,
+                                            marginLeftArrowPrevious: 12,
+                                            marginRightArrowNext: 12,
+                                            marginTopArrowNext: 12,
+                                            marginTopArrowPrevious: 12,
+                                            decorationDateSelected:
+                                                BoxDecoration(
+                                                    color: Color.fromRGBO(
+                                                        25, 50, 60, 0.85),
+                                                    shape: BoxShape.circle,
+                                                    border: Border.all(
+                                                        color: Colors.white,
+                                                        width: 1)),
+                                            backgroundPicker: Color.fromRGBO(
+                                                25, 50, 60, 0.85),
+                                            backgroundActionBar: Color.fromRGBO(
+                                                25, 50, 60, 0.85),
+                                            backgroundHeaderMonth:
+                                                Color.fromRGBO(
+                                                    25, 50, 60, 0.85),
+                                          ),
+                                          styleYearPicker:
+                                              MaterialRoundedYearPickerStyle(
+                                            textStyleYear: TextStyle(
+                                                fontSize: 50,
+                                                color: Colors.white),
+                                            heightYearRow: 0,
+                                          ),
+                                          initialDate: DateTime.now(),
+                                          firstDate:
+                                              DateTime(DateTime.now().year),
+                                          lastDate:
+                                              DateTime(DateTime.now().year + 1),
+                                          borderRadius: 16,
+                                        ),
                                         /*{
                                           showDialog(
                                             context: context,
